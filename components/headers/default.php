2be7871fc61b79135a73a6fa38544d3557852f60
Leônidas Santos <oi@ltco.com.br>
2020-12-22 09:25:43 -0300

    fix(header): correction and addition of class

--- components/headers/default.php
+++ components/headers/default.php
@@ -1,8 +1,9 @@
 <?php
   if ( has_excerpt() ) $excerpt = get_the_excerpt();
+  if ( has_post_thumbnail() ) $ltco_has_thumb = 'ltco_has_thumb';
 ?>
 
-<header class="header default" <?= ltco_thumbnail_post(); ?>>
+<header class="header default <?= $ltco_has_thumb ?>" <?= ltco_thumbnail_post(); ?>>
   <div class="container ltco_title_header">
     <h1><?= ltco_title(); ?></h1>
     <p class="mb-0"><?= $excerpt; ?></p>
